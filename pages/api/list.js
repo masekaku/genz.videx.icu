a11eebcadb0974f68c819428c8ebadc1c863c9a4
masekaku <aguslistiyanto27@gmail.com>
2025-11-19 23:49:29 +0700

    Create list.js

---
+++ pages/api/list.js
@@ -0,0 +1,7 @@
+import videosData from '../../videos.json';
+
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+  return res.json(videosData);
+}
